b6821c71e417e4582261ec3e4f7f4124a5a3b5cf
vadim_budarin <budarin-vv@yandex.ru>
2022-01-16 10:24:18 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # plpgsql-ulid-generator
-Function for generating ULID as UUID on  plpgsql
+
+PostgreSQL function for generating ULID as UUID on  plpgsql
+
+```sql
+select ulid();                          --017e61c1-d833-4af7-70c7-8444ae1d207a
+select ulid('2022-01-16 05:48:00');     --017e616d-9a80-0000-0000-000000000000
+```
+
+Used for monotonic ids and for partitioning by it
+
+```plpgsql
+CREATE TABLE "table" (
+  id text primary key not null default generate_ulid(now(), true),
+  ...
+) partition by range (id);
+
+CREATE TABLE table_2021 partition of "table" for values
+  from
+    (ulid('2021-01-01'))
+  to
+    (ulid('2022-01-01'));
+```
---
+++ ulid.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION ulid(dt timestamptz = NULL)
+RETURNS uuid
+AS $$
+DECLARE
+  -- Crockford's Base32
+  encoding   BYTEA = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+  timestamp  BYTEA = E'\\000\\000\\000\\000\\000\\000';
+  output     uuid;
+
+  unix_time  BIGINT;
+  ulid       BYTEA;
+  now		 timestamptz;
+BEGIN
+  if (dt is NULL) then
+    now = current_timestamp;
+  else
+  	now = dt;
+  end if;
+
+  -- 6 timestamp bytes
+  unix_time = (EXTRACT(EPOCH FROM now) * 1000)::BIGINT;
+  timestamp = SET_BYTE(timestamp, 0, (unix_time >> 40)::BIT(8)::INTEGER);
+  timestamp = SET_BYTE(timestamp, 1, (unix_time >> 32)::BIT(8)::INTEGER);
+  timestamp = SET_BYTE(timestamp, 2, (unix_time >> 24)::BIT(8)::INTEGER);
+  timestamp = SET_BYTE(timestamp, 3, (unix_time >> 16)::BIT(8)::INTEGER);
+  timestamp = SET_BYTE(timestamp, 4, (unix_time >> 8)::BIT(8)::INTEGER);
+  timestamp = SET_BYTE(timestamp, 5, unix_time::BIT(8)::INTEGER);
+
+  -- 10 entropy bytes
+  if (dt is NULL) then
+  	ulid = timestamp || gen_random_bytes(10);
+  else
+	ulid = timestamp || '\x00000000000000000000';
+  end if;
+
+  output = CAST(substring(ulid::text from 3) AS uuid);
+
+RETURN output;
+END
+$$
+LANGUAGE plpgsql
+VOLATILE;
